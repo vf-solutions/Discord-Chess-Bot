dc43d27c508531f4020b031a84cbe29c18cc28f2
Kaweees <49963287+Kaweees@users.noreply.github.com>
2021-03-02 11:54:49 -0800

    Clean up code

--- bot.py
+++ bot.py
@@ -1,161 +1,31 @@
 # invite link:
 # https://discord.com/api/oauth2/authorize?client_id=744024313476415540&permissions=8&scope=bot
+
 import discord
-from discord.ext import commands
 import json
-from constants import *
-#print(discord._version)
+print(discord._version)
 
-token = "NzQ0MDI0MzEzNDc2NDE1NTQw.XzdMow.aa_S8fu3GCs6zayAr4EUHVZT0eU"
-client = commands.Bot(command_prefix = '$')
-client.remove_command("$help")
+client = discord.Client()
 
+@client.event
+async def on_ready(self):
+    await client.change_presence(status=discord.Status.online, activity=discord.Game(name="Chess :D"))
+    print("My body is ready")
+    print('We have logged in as {0.user}'.format(client))
 
-class  MyBot(commands.Bot):
-    # Loads the Config.json File so stuff can be easilly Changed instead of having to return to the code every time
-    with open('./config.json') as f:
-        config = json.load(f)
-    n_channel_id = int(config['names-channel-id'])
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
 
-    async def on_ready(self):
-        await client.change_presence(status=discord.Status.online, activity=discord.Game(name="React to my messages in #select-roles to show others the classes you are in"))
-        # with open('assets/img/fox.jpg', 'rb') as f:
-        #     await client.edit_profile(avatar=f.read())
-        print("My body is ready")
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
 
-    async def on_raw_reaction_add(self, payload):
-        message_id = payload.message_id
-        if payload.user_id != 744024313476415540: #checks if reaction is from bot
-        print("Add role initiated")
-        guild_id = payload.guild_id
-        guild = discord.utils.find(lambda g : g.id == guild_id, client.guilds)
-        value = payload.emoji.name
-        # print(value) used in debugging
-        global all_roles_list
-        for i in range(len(all_roles_list)):
-            if all_roles_list[i] in value:
-                role = discord.utils.get(guild.roles, name=all_roles_dict[all_roles_list[i]])
-                print(role)
-                break
-        if role is None:
-            print("Role not found")
-            await guild.create_role(name=all_roles_dict[all_roles_list[i]], mentionable=True)
-            for i in range(len(all_roles_list)):
-                if all_roles_list[i] in value:
-                    role = discord.utils.get(guild.roles, name=all_roles_dict[all_roles_list[i]])
-                    print(role)
-                    break
-        member = discord.utils.find(lambda m : m.id ==  payload.user_id, guild.members)
-        if member:
-            await role.edit(mentionable=True)
-            await member.add_roles(role)
-            print("success")
-        else:
-            print("Member not found")
-        else:
-        print("The bot reacted")
-        print(payload.user_id)
-    
-    async def on_raw_reaction_remove(self, payload):
-        message_id = payload.message_id
-        if payload.user_id != 744024313476415540: #checks if reaction is from bot
-            print("Add role initiated")
-            guild_id = payload.guild_id
-            guild = discord.utils.find(lambda g : g.id == guild_id, client.guilds)
-            value = payload.emoji.name
-            # print(value) used in debugging
-            global all_roles_list
-            for i in range(len(all_roles_list)):
-                if all_roles_list[i] in value:
-                    role = discord.utils.get(guild.roles, name=all_roles_dict[all_roles_list[i]])
-                    print(role)
-                    break
-            if role is not None:
-                member = discord.utils.find(lambda m : m.id ==  payload.user_id, guild.members)
-                if member:
-                    await member.remove_roles(role)
-                    print("success")
-                else:
-                    print("Member not found")
-            else:
-                print("Role not found")
-        else:
-            print("The bot removed a react")
-            print(payload.user_id)
+def getToken():
+    # code to open and read token
+    with open('assts/token.txt', 'r') as file: # read file content
+        data = file.read().replace('\n', '')
+    return data # store file contents in data
 
-    async def on_message(self, message):
-        if message.content.lower().find("hello there") != -1:
-            await message.channel.send("General Kenobi") # If the user says !hello we will send back hi
-        elif message.content.lower().find("i don't like sand") != -1:
-            channel = message.channel
-            await message.add_reaction("🏖️")
-            await message.add_reaction("⏳")
-            await message.add_reaction("🏜️")
-            filename = "assets/img/sand.jpg"
-            await channel.send(file=discord.File(filename))
-            print(f"File '{filename}' was sent on {message.channel}")
-        # If the Message was sent to The Names Channel Specified in the config.json then the user's Nickname is Changed to it.
-        # TODO: Also Assigns Specified Role in Config.json
-        elif message.channel.id == MyBot.n_channel_id:
-            await message.author.edit(nick=message.content)
-        await client.process_commands(message)
-        if (message.author.bot) and (message.author.id == client.user.id): #checks if message is from bot
-            global all_roles_list
-            print(message.embeds)
-            for embed in (message.embeds):
-                for field in embed.fields:
-                    # print(field.name)
-                    print(field.value)
-                    value = field.value
-                    for i in range(len(all_roles_list)):
-                        if all_roles_list[i] in value:
-                            await message.add_reaction(all_roles_list[i])
-        else:
-            print(message.author.name)
-    
-    async def on_member_join(member):
-        guild = member.guild
-        print(guild)
-        rules = guild.get_channel(633477240797134870)
-        pick_roles = guild.get_channel(745093331318734989)
-        names = guild.get_channel(633490755889135626)
-        channel = guild.get_channel(633473337766576139)
-        await channel.send(f'Welcome {member.mention}! Make sure to check {rules.mention} for server rules and {pick_roles.mention} for class roles. Also change your nickname to your irl name in {names.mention}.')
-
-
-
-@client.command()
-async def setroles(ctx):
-    channel = ctx.message.channel
-    if ctx.message.author.guild_permissions.administrator:
-        async for message in channel.history(limit=200): # clears all of the bot messages in the channel
-            if message.author == client.user:
-                await message.delete()
-        await ctx.message.delete()
-        global special_roles
-        # TODO remove previous messages the bot sent in the channel
-        for key in special_roles.keys():
-            embed=discord.Embed(color=0xFF0000)
-            embed.add_field(name=f"Role Menu: {key}", value="React to give yourself a role.", inline=False)
-            for subkeys in special_roles[key].keys():
-                embed.add_field(name="\u200b", value=f'{subkeys} : {special_roles[key][subkeys]}', inline=False)
-            await ctx.send(embed=embed)
-    else:
-        msg = f"Sorry {ctx.message.author.mention}, only Admins can use this command"
-        await channel.send(msg)
-
-@client.command()
-async def test(ctx, *, arg):
-    print(arg)
-    await ctx.send(arg)
-
-
-@client.command()
-async def ping(ctx):
-    print(ctx)
-    await ctx.send(f'Pong! {round(client.latency * 1000)} ms')
-
-
-
-bot = MyBot(command_prefix = '$')
-bot.run(token)
+client = discord.Client()
+client.run(getToken())
